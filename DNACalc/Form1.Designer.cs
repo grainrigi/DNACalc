ea78549951be3e2b7008c90b732e44714c66d0ad
grainrigi <naonaokiryu2@outlook.com>
2021-03-30 15:06:56 +0900

    Fix checkbox styling

--- DNACalc/Form1.Designer.cs
+++ DNACalc/Form1.Designer.cs
@@ -207,6 +207,7 @@
             // 
             // checkBox1
             // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.checkBox1.AutoSize = true;
             this.checkBox1.Location = new System.Drawing.Point(77, 396);
             this.checkBox1.Name = "checkBox1";
